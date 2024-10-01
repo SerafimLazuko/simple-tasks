bfc76713641aba69f5085054824393b3bed4d26e
Lazuko Serafim <32098170+SerafimLazuko@users.noreply.github.com>
2024-10-01 11:10:19 +0300

    Create IntArraysCommonNumber.cs
    Найти число, которое присутствует во всех трёх массивах.

---
+++ IntArraysCommonNumber.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+//Даны три неубывающих массива чисел. Найти число, которое присутствует во всех трех массивах.
+//Input: [1,2,4,5], [3,3,4], [2,3,4,5,6]
+//Output: 4
+
+public class Program
+{
+	public static void Main()
+	{
+		int[] arr1 = new int[4]{1,2,4,5};
+		int[] arr2 = new int[3]{3,3,4};
+		int[] arr3 = new int[5]{2,3,4,5,6};
+		
+		List<int[]> arrays = new List<int[]>();
+		arrays.Add(arr1);
+		arrays.Add(arr2);
+		arrays.Add(arr3);
+		
+		Console.WriteLine(CommonNum(arrays));
+	}
+	
+	public static int CommonNum(List<int[]> arrays)
+	{
+		arrays.Sort((x,y) => x.Length.CompareTo(y.Length));
+		
+		var longest = arrays[0];
+		var hashSetIntersetcResult = new HashSet<int>(longest);
+		
+		foreach(var arr in arrays)
+		{
+			hashSetIntersetcResult.IntersectWith(arr);
+		}
+		
+		var enumerator = hashSetIntersetcResult.GetEnumerator();
+		enumerator.MoveNext();
+		
+		return hashSetIntersetcResult.Count > 0 ? enumerator.Current : 0;
+	}
+}
